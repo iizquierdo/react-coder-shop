b6e365949b789f1ac46a7d90e81a9d9f84f02928
Ignacio Izquierdo <ignaciojavierizquierdo@gmail.com>
2021-02-08 20:37:16 -0300

    ItemCount Component

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const ItemCountComponent = ({ stock, onAdd, onSubstract, contador }) => {
+    return(
+    <>
+
+    <div class="input-group">
+        <span class="input-group-btn">
+            <button className="btn btn-danger btn-number" onClick={() => { onSubstract(stock) }}>-</button> 
+        </span>
+        
+        <span className="form-control">{contador}</span>
+        
+        <span class="input-group-btn">
+            <button className="btn btn-danger btn-number"  onClick={() => { onAdd(stock) }}>+</button> 
+        </span>
+    </div>
+    <br/>
+    <a href="#" className="btn btn-danger btn-sm btn-block">Agreagar al Carrito</a>
+    
+
+      </>
+    );
+}
+
+
+export default ItemCountComponent;
--- src/containers/ItemListContainer.jsx
+++ src/containers/ItemListContainer.jsx
@@ -1,9 +1,54 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ItemCountComponent from '../components/ItemCount';
+import React, { useState } from 'react';
 
 const ItemListContainerComponent = ({greeting}) => {
-    return(
-    <>
-        <a href="#" className="btn btn-danger btn-sm">{greeting}</a>
+
+  const [contador, setContador] = useState(1); 
+   
+  const onAdd = (stock) =>{
+    if(contador < stock) {
+      setContador(contador + 1);
+    }
+  }
+
+  const onSubstract = (stock) =>{
+    if(contador > 0) {
+      setContador(contador - 1);
+    }
+  }
+  
+  
+  
+  return(
+      <>
+
+<div class="container col-4">
+  <div class="row">
+    <div class="col-6">
+      <div class="card-deck">
+        <div class="card">
+          <div class="card-body d-flex align-items-end flex-column">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">
+                <h5 class="card-title">TITULO DE EJEMPLO</h5>
+                <p class="card-text">Descripcion de ejemplo Descripcion de ejemplo Descripcion de ejemplo Descripcion de ejemplo Descripcion de ejemplo </p>
+              </li>
+            </ul>
+           
+              
+            
+              <ItemCountComponent stock={12} contador={contador} onAdd={onAdd} onSubstract={onSubstract}/>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+         
       </>
     );
 }
